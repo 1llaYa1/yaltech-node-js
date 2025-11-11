75f32620ba976ca7d9885c7ac8a7be0ea78dafe0
1llaYa1 <asgat.yalaev@gmail.com>
2025-11-11 20:15:54 +0300

    a

--- server.js
+++ server.js
@@ -37,7 +37,8 @@ app.get('/clients', async (req, res) => {
     await addClientToClientsTable(req.body.fullname, req.body.email, req.body.comment);
 });*/
 
-app.post('/clients', async (req, res) => addClientToClientsTable());
+app.post('/clients', async (req, res) => {
+    await addClientToClientsTable(req.body.fullname, req.body.email, req.body.comment)});
 
 app.get('/cleardb', async (req, res) => {
     trunkateClientsTable();
