5394675396f6049cd6e35cc85fdfd938eac7366e
1llaYa1 <asgat.yalaev@gmail.com>
2025-11-11 10:45:31 +0300

    2

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ import fs from 'fs'
 import http from 'http'
 import https from 'https'
 
-var privateKey  = fs.readFileSync('sslsert/server.key', 'utf8');
-var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
+//var privateKey  = fs.readFileSync('sslsert/server.key', 'utf8');
+//var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
 
-var credentials = {key: privateKey, cert: certificate};
+//var credentials = {key: privateKey, cert: certificate};
 
 import { getClientsTableContents, addClientToClientsTable, testConnection } from './db.js'
 
